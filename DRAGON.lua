d8ca9da32c7398b4fc19eea7084c2410c6b39aed
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-04-11 18:07:05 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -9622,6 +9622,7 @@ x = 0
 local list = DRAGON.members_
 for k, v in pairs(list) do
 x = x + 1
+chat_kick(msg.chat_id_,v.user_id_)
 if database:get(bot_id..'user:Name'..v.user_id_) then
 t = t..""..x.." → {[@"..database:get(bot_id..'user:Name'..v.user_id_).."]}\n"
 else
