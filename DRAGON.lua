030c34a76c8fdff2aa4142f676fab3a336a5594b
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-04-18 02:46:03 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -8175,7 +8175,7 @@ keyboard.inline_keyboard = {
 {
 {text = '❶️❶', callback_data="/game1"},{text = '❷❷', callback_data="/game2"},
 },
-{
+}
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
