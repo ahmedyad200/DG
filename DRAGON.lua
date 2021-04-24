e9a9e0592b19806da2f99828a0d041d7891674e5
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-04-25 00:48:02 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -11121,7 +11121,7 @@ Text = ' ☉┇ الشخص الذي قام باضافتك هو » '..Name
 sendText(msg.chat_id_,Text,msg.id_/2097152/0.5,'md')
 end,nil)
 else
-send(msg.chat_id_, msg.id_,' ☉┇ انت دخلت عبر الرابط لتلح') 
+send(msg.chat_id_, msg.id_,' ☉┇ انت دخلت عبر الرابط') 
 end
 end,nil)
 else
@@ -11205,7 +11205,7 @@ send(msg.chat_id_, msg.id_,'لا تمتلك صوره في حسابك', 1, 'md')
 tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = msg.sender_user_id_, offset_ = 0, limit_ = 1 }, getpro, nil)
 end
 
-if text == "أحمد"  then
+if text == "أحمد" or text == 'احمد' then
 database:set(bot_id.."Ahmed_photo:status"..msg.chat_id_,true) 
 local AY_ph = database:get(bot_id.."Ahmed_photo:status"..msg.chat_id_)
 if not AY_ph then
@@ -11216,7 +11216,7 @@ local function getpro(extra, result, success)
 if result.photos_[0] then
 sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[0].sizes_[1].photo_.persistent_id_," أحمد مبرمج سورس باور @AY_AHD ", msg.id_, msg.id_, "md")
 else
-send(msg.chat_id_, msg.id_,'فشل الاتصال', 1, 'md')
+send(msg.chat_id_, msg.id_,'أحمد مبرمج سورس باور @AY_AHD', 1, 'md')
   end end
 tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = 944353237, offset_ = 0, limit_ = 1 }, getpro, nil)
 end
