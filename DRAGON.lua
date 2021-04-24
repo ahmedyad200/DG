d29bf52cb8be407c650805c1788f97552e6fde06
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-04-25 00:56:03 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -12856,13 +12856,6 @@ end
 
 ---------------------- الاوامر الجديده
 if text == 'الاوامر' then
-if result.photos_[0] then
-sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[0].sizes_[1].photo_.persistent_id_," ", msg.id_, msg.id_, "md")
-else
-
-  end end
-tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = bot_id, offset_ = 0, limit_ = 1 }, getpro, nil)
-end
 if not Mod(msg) then
 send(msg.chat_id_, msg.id_,'')
 return false
