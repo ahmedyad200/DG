d1f73256bb4fa69b463fab6c406cd3d3406afa82
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-04-26 02:46:39 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -296,7 +296,7 @@ else
 return false 
 end 
 end
-function memp(msg)
+function Memp(msg)
 local hash = database:sismember(bot_id..'Memp:User'..msg.chat_id_,msg.sender_user_id_) 
 if hash or Sudo(msg) or BasicConstructor(msg) or Constructor(msg) or Manager(msg) or Gmod(msg) or Mod(msg) or CoSu(msg) or Bot(msg)  then       
 return true 
