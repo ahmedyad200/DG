f2b0713848dfd048c67ac16e6a298570443c3d88
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-05-05 23:12:13 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -995,7 +995,7 @@ end
 send_inline_key(msg.chat_id_,Start_Source,nil,inline)
 send_inline_key(msg.chat_id_,Sudo_Welcome,Keyboard)
 end
-database:setex(bot_id..'Start:Time'..msg.sender_user_id_,10,true)
+database:setex(bot_id..'Start:Time'..msg.sender_user_id_,1,true)
 return false
 end end
 if not DevSoFi(msg) and not database:sismember(bot_id..'Ban:User_Bot',msg.sender_user_id_) and not database:get(bot_id..'Tuasl:Bots') then
