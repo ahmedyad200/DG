f708ad198f32b815c77cec65d1f28663a018906d
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-05-16 05:13:56 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -4635,7 +4635,7 @@ local Text = [[
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '𝘾𝙃𝘼𝙉𝙉𝙀𝙇', url="t.me/SOPOWERB0T"}},
-{{text = 'اضف البوت الي مجموعتك', url="http://t.me/"..UserBot.."?startgroup=AHMEDYAD"}},
+{{text = 'اضف البوت الي مجموعتك', url="http://t.me/".. UserBot .."?startgroup=AHMEDYAD"}},
 } 
 local msg_id = msg.id_/2097152/0.5 
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
