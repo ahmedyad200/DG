6f83d1673d77c6b4d0646140ee57dadcec233a1e
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-05-16 06:44:46 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -4627,7 +4627,6 @@ send(msg.chat_id_, msg.id_,' جاري ارسال السورس الي المبر�
 end
 if text == 'السورس' or text == 'سورس' or text == 'يا سورس' then 
 local UBOT = database:get(id_server.."USERBOT"),
-local msg_id = msg.id_/2097152/0.5 
 local Text = [[ 
 ☉┇ 𝐒𝐎𝐔𝐑𝐂𝐄 𝐏𝐎𝐖𝐄𝐑 ┇☉
 •┉•┉•┉┉•┉•┉••┉•┉•┉┉•┉•┉•
@@ -4642,6 +4641,7 @@ local Text = [[
 ]]
 keyboard = {}
 keyboard.inline_keyboard = {{{text = '𝘾𝙃𝘼𝙉𝙉𝙀𝙇', url="t.me/SOPOWERB0T"}},{{text = 'اضف البوت الي مجموعتك', url="http://t.me/".. UBOT .."?startgroup=start"}},} 
+local msg_id = msg.id_/2097152/0.5 
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 --------------------------------------------------------------------------------------------------------------
