cf5198dd58c95efac9f98ea6ee86f471713571f2
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-05-16 05:50:48 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -27,7 +27,7 @@ end
 file:write(serialized)    
 file:close()  
 end  
---[[if not database:get(id_server..":IDSUDO") then
+if not database:get(id_server..":USERBOT") then
 io.write('\27[38;5;77m\n                •{ installd source power }•\n\27')
 print([[
 
@@ -48,18 +48,7 @@ print([[
  
  
  
-]) خطأ
-io.write('\27[0;35m\n ارسل لي ايدي المطور الاساسي ↓ :\n•┉ • ┉ • ┉ 𝔓𝔒𝔚𝔈ℜ ┉ • ┉ • ┉•\n\27[0;33;49m')
-local SUDOID = io.read()
-if SUDOID ~= '' then
-io.write('\27[1;35m تم حفظ ايدي المطور الاساسي \n•┉ • ┉ • ┉ 𝔓𝔒𝔚𝔈ℜ ┉ • ┉ • ┉•\n27[0;39;49m')
-database:set(id_server..":IDSUDO",SUDOID)
-else
-print('\27[0;31m•┉ • ┉ • ┉ 𝔓𝔒𝔚𝔈ℜ ┉ • ┉ • ┉• ━ ━\n لم يتم حفظ ايدي المطور الاساسي ارسله مره اخره')
-end 
-os.execute('lua DRAGON.lua')
-end]]--
-if not database:get(id_server..":USERBOT") then
+]])
 io.write('\27[1;31m ↓ ارسل معرف البوت بدون @ :\n SEND USER FOR BOT : \27[0;39;49m')
 local BOTUSERNAME = io.read():gsub('@','')
 if BOTUSERNAME ~= '' then
@@ -67,6 +56,17 @@ io.write('\n\27[1;34m تم حفظ معرف البوت :\n\27[0;39;49m')
 database:set(id_server..":USERBOT",'@'..BOTUSERNAME)
 else
 print('\n\27[1;34m لم يتم حفظ معرف البوت :')
+end 
+os.execute('lua DRAGON.lua')
+end
+if not database:get(id_server..":IDSUDO") then
+io.write('\27[0;35m\n ارسل لي ايدي المطور الاساسي ↓ :\n•┉ • ┉ • ┉ 𝔓𝔒𝔚𝔈ℜ ┉ • ┉ • ┉•\n\27[0;33;49m')
+local SUDOID = io.read()
+if SUDOID ~= '' then
+io.write('\27[1;35m تم حفظ ايدي المطور الاساسي \n•┉ • ┉ • ┉ 𝔓𝔒𝔚𝔈ℜ ┉ • ┉ • ┉•\n27[0;39;49m')
+database:set(id_server..":IDSUDO",SUDOID)
+else
+print('\27[0;31m•┉ • ┉ • ┉ 𝔓𝔒𝔚𝔈ℜ ┉ • ┉ • ┉• ━ ━\n لم يتم حفظ ايدي المطور الاساسي ارسله مره اخره')
 end 
 os.execute('lua DRAGON.lua')
 end
@@ -81,7 +81,7 @@ print('\n\27[1;34m لم يتم حفظ معرف المطور :')
 end 
 os.execute('lua DRAGON.lua')
 end
---[[if not database:get(id_server..":token") then
+if not database:get(id_server..":token") then
 io.write('\27[0;31m\n ارسل لي توكن البوت الان ↓ :\n•┉ • ┉ • ┉ 𝔓𝔒𝔚𝔈ℜ ┉ • ┉ • ┉•\n\27')
 local token = io.read()
 if token ~= '' then
@@ -96,7 +96,7 @@ else
 print('\27[0;35m•┉ • ┉ • ┉ 𝔓𝔒𝔚𝔈ℜ ┉ • ┉ • ┉• ━\n لم يتم حفظ التوكن ارسل لي التوكن الان')
 end 
 os.execute('lua DRAGON.lua')
-end]]--
+end
 local create_config_auto = function()
 config = {
 token = database:get(id_server..":token"),
